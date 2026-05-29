e93686c7321e50d71310d1d6c21d9c93bd43b5e8
shrqbabu <techloot1100@gmail.com>
2026-05-29 13:35:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -71,7 +71,7 @@ export default function App() {
                 <Route path="/games/dragon-tiger" element={<DragonTigerPage />} />
                 <Route path="/games/andar-bahar" element={<AndarBaharPage />} />
                 <Route path="/games/poker" element={<PokerLobbyPage />} />
-                <Route path="/pocker/:tableId" element={<ProtectedRoute><PokerGamePage /></ProtectedRoute>} />
+                <Route path="game/pocker/:tableId" element={<ProtectedRoute><PokerGamePage /></ProtectedRoute>} />
                 
                 {/* Admin routes */}
                 <Route element={<AdminRoute />}>
